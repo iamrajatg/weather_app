9f5cb6f8b986dc88f8056e73454cd911f75bdcfd
Rajat Gupta <guptarajat846@gmail.com>
2020-01-29 11:42:30 +0530

    Changed Fahrenheit to celcius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            const celciusTemp = (body.currently.temperature - 32)*5/9
+            const roundedCelcius = celciusTemp.toFixed(2)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + roundedCelcius + ' degrees celcius. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
